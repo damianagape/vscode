13e967dc4bf737f192bebde7d6a53b3c28a3c44e
damianagape <damian.agape@gmail.com>
2025-11-22 23:52:54 +0100

    notes

--- scripts/dump-macos.bash
+++ scripts/dump-macos.bash
@@ -4,3 +4,5 @@ set -euf -o pipefail
 code --list-extensions > ./macos/extensions.txt
 cp -f "$HOME/Library/Application Support/Code/User/settings.json" ./macos/settings.json
 cp -f "$HOME/Library/Application Support/Code/User/keybindings.json" ./macos/keybindings.json
+
+cp -f "$HOME/Library/Mobile Documents/com~apple~CloudDocs/dagape-icloud/dagape-notes/.vscode/settings.json" ./macos/icloud-notes/settings.json
